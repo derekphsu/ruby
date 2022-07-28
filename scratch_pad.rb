0df2cd1e974321a49ba889cd08a23740d0c3e0a6
derekphsu <hsu4derek@gmail.com>
2022-07-27 17:03:24 -0700

    testing

--- scratch_pad.rb
+++ scratch_pad.rb
@@ -5,8 +5,9 @@ require 'net/http'
 def curling
 	uri = URI("https://pokeapi.co/api/v2/pokemon/ditto")
 	res = Net::HTTP.get_response(uri)
-	puts res.methods
+	puts res.is_a?(Net::HTTPError)
 	puts res.get_response
+	puts "random"
 	# puts res.body
 	# puts res.is_a?(Net::HTTPError)
 end
